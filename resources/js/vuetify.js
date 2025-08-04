12d518cd8e5addcdf02c154d4c3b211478a1c0c9
iliyashev <iliya.shevolaev@gmail.com>
2025-08-04 12:13:03 +0300

    ru locale

--- resources/js/vuetify.js
+++ resources/js/vuetify.js
@@ -1,7 +1,12 @@
 import { createVuetify } from 'vuetify'
-
+import { ru } from 'vuetify/locale'
 
 export default createVuetify({
+    locale: {
+        locale: 'ru',
+        fallback: 'ru',
+        messages: { ru },
+      },
     theme: {
       defaultTheme: 'dark',
     },
